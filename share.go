60cd24b1b0f3ba70f63a9968ef29b832d1bf451b
nanachi-sh <nanachi.sh@protonmail.com>
2025-10-17 23:51:20 +0800

    upload

---
+++ share.go
@@ -0,0 +1,44 @@
+package main
+
+type Sender interface {
+	Send([]byte)
+}
+
+type Builder interface {
+	SendGroupMessage(group_id string, contents ...Content) []byte
+}
+
+type Message interface {
+	OfGroup() MessageGroup
+}
+
+type MessageGroup interface {
+	GetContents() []Content
+	GetBotID() string
+	GetTimestamp() int64
+	GetMessageId() string
+	GetSender() MessageSender
+	GetGroupID() string
+}
+
+type MessageSender struct {
+	ID   string
+	Name string
+}
+
+type Content struct {
+	OfText *TText
+	OfAt   *TAt
+}
+
+type TText string
+
+type TAt string
+
+func Text(text string) Content {
+	return Content{OfText: (*TText)(&text)}
+}
+
+func At(user_id string) Content {
+	return Content{OfAt: (*TAt)(&user_id)}
+}
